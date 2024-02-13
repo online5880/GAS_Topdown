a269a48bcdf386a6b8d65d9c50330ccc2b8c3e37
online5880 <gjtjqkr1@naver.com>
2024-02-14 00:37:16 +0900

    Mana Changed

--- Source/Aura/Private/UI/WidgetController/OverlayWidgetController.cpp
+++ Source/Aura/Private/UI/WidgetController/OverlayWidgetController.cpp
@@ -12,6 +12,8 @@ void UOverlayWidgetController::BroadcastInitialValues()
 	{
 		OnHealthChanged.Broadcast(AuraAttributeSet->GetHealth());
 		OnMaxHealthChanged.Broadcast(AuraAttributeSet->GetMaxHealth());
+		OnManaChanged.Broadcast(AuraAttributeSet->GetMana());
+		OnMaxManaChanged.Broadcast(AuraAttributeSet->GetMaxMana());
 	}
 }
 
@@ -24,6 +26,12 @@ void UOverlayWidgetController::BindCallbacksToDependencies()
 
 		AbilitySystemComponent->GetGameplayAttributeValueChangeDelegate(
 			AuraAttributeSet->GetMaxHealthAttribute()).AddUObject(this, &ThisClass::MaxHealthChanged);
+		
+		AbilitySystemComponent->GetGameplayAttributeValueChangeDelegate(
+			AuraAttributeSet->GetManaAttribute()).AddUObject(this, &ThisClass::ManaChanged);
+
+		AbilitySystemComponent->GetGameplayAttributeValueChangeDelegate(
+			AuraAttributeSet->GetMaxManaAttribute()).AddUObject(this, &ThisClass::MaxManaChanged);
 	}
 }
 
@@ -35,4 +43,14 @@ void UOverlayWidgetController::HealthChanged(const FOnAttributeChangeData& Data)
 void UOverlayWidgetController::MaxHealthChanged(const FOnAttributeChangeData& Data) const
 {
 	OnMaxHealthChanged.Broadcast(Data.NewValue);
+}
+
+void UOverlayWidgetController::ManaChanged(const FOnAttributeChangeData& Data) const
+{
+	OnManaChanged.Broadcast(Data.NewValue);
+}
+
+void UOverlayWidgetController::MaxManaChanged(const FOnAttributeChangeData& Data) const
+{
+	OnMaxManaChanged.Broadcast(Data.NewValue);
 }
